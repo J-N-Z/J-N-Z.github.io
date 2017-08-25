f26c3f6cbdb77dfb6336f283626209e1d49da6b8
Kolchevskiy Pavel <pkolchevskiy@mail.ru>
2017-08-25 16:43:15 +0300

    Added 'Numbers' section and contacts

--- js/main.js
+++ js/main.js
@@ -1,36 +1,5 @@
 ;(function() {
 
-	$('.js-main-slider').owlCarousel({
-		loop: true,
-		responsive:{
-			0:{
-				items:1
-			}
-		}
-	});
 
-	$('.js-trending-slider').owlCarousel({
-		loop: true,
-		responsive:{
-			0:{
-				items:1
-			}
-		}
-	});
-
-	$('.js-blog-slider').owlCarousel({
-		nav:true,
-		dots: false,
-		loop: true,
-		margin: 15,
-		responsive:{
-			0:{
-				items:1
-			},
-			768:{
-				items:2
-			}
-		}
-	});
 })();
 
