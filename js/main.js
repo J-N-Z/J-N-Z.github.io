0d0545af8c763d8ec97871743c62ab9f5fb83be7
Kolchevskiy Pavel <pkolchevskiy@mail.ru>
2017-08-27 20:09:37 +0300

    added adaptive menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 ;(function() {
 
+    var menu = $('.main-menu'),
+        menuToggler = $('.main-menu__toggle'),
+        menuList = $('.main-menu__list');
+
+    menuToggler.on('click', function() {
+        if (!menu.hasClass('main-menu_active')) {
+            menu.addClass('main-menu_active');
+            menuList.stop().slideDown();
+        } else {
+            menu.removeClass('main-menu_active');
+            menuList.stop().slideUp();
+        }
+    })
 
 })();
 
